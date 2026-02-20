ca24ae409932f77c5f2f907c5520a608c2b92dea
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-20 08:39:08 -0800

    initial commit, standard format added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace COMP003A.Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
